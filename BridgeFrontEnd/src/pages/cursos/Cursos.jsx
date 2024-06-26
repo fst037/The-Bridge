a0e8f7958fa0247e68bf6c8444827b1a932e7458
DonnaIsabel97 <93611558+DonnaIsabel97@users.noreply.github.com>
2024-06-26 16:47:38 -0300

    Update Cursos.jsx

--- BridgeFrontEnd/src/pages/cursos/Cursos.jsx
+++ BridgeFrontEnd/src/pages/cursos/Cursos.jsx
@@ -1,6 +1,34 @@
+import React, { useState } from 'react';
+import { Modal } from "../../components/Modal";
+import CreateCourseModal from './CreateCourseModal'; 
+import { AddActionButton } from "../../components/AddActionButton";
+import { useCardToggle } from '../../hooks/useCardToggle';
+
+
 export const Cursos = () => {
-  <div className="p-8">
-    <h2 className="text-4xl text-gray-400/80">Cursos</h2>
-    <main></main>
-  </div>;
+  const {isOpen, setIsOpen, cardRef} = useCardToggle();
+
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
+
+  const handleCreateCourse = (courseName) => {
+    console.log(`Curso creado: ${courseName}`);
+  };
+
+  return (
+    <section>
+      <div className="p-8">
+        <div className="flex items-center gap-4">
+          <h3 className="text-4xl text-gray-400/80">Mis Cursos</h3>
+          <AddActionButton text={"Agregar +"} className="w-max" onClick={openModal} />
+          <CreateCourseModal isOpen={isOpen} setIsOpen={setIsOpen} onSubmit={handleCreateCourse} />
+        </div>
+        <article className="grid grid-cols-[repeat(auto-fit,_minmax(400px,_1fr))] gap-4 my-4">
+          <p>Curso 1</p>
+          
+        </article>
+      </div>
+      
+    </section>
+  ) 
 };
