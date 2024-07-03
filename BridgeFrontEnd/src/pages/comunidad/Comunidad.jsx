2787bb4d97b8b6f54a9fc4a21082780607ed5928
VidArg86 <tomasfarao03@gmail.com>
2024-07-03 17:00:10 -0300

    Terminada las pantallas de comunidad y arreglado algunos errores
    Cambiada la imagen a la ultima ver, y terminadas las pantallas de Comunidad

---
+++ BridgeFrontEnd/src/pages/comunidad/Comunidad.jsx
@@ -0,0 +1,122 @@
+import React from 'react';
+export const Comunidad = () => {
+  return (
+    <div className="p-8">
+      <h2 className="text-4xl text-gray-400/80">Comunidad</h2>
+      <div className="Conocidos left-[117px] top-[215px] absolute text-black text-4xl font-medium font-['Inter']">Compañeros</div>
+      <div className="Mutuos left-[117px] top-[430px] absolute text-black text-4xl font-medium font-['Inter']">Builders</div>
+      <div className="Recomendados left-[117px] top-[650px] absolute text-black text-4xl font-medium font-['Inter']">Recomendados</div>
+      <div className="VerMas1 w-1219 h-8 top-[250px] relative">
+        <div className="Linea w-1219 h-px border border-neutral-400"></div>
+        <div className="EspacioBoton left-[835px] top-[-15px] relative">
+          <a href="conocidos">
+          <button style={{ backgroundColor: '#00BCC6', color: 'white' }}>+ Ver Más</button>
+          </a>
+        </div>
+      </div>
+      <div className="VerMas2 w-1219 h-8 top-[435px] relative">
+        <div className="Linea w-1219 h-px border border-neutral-400"/>
+        <div className="EspacioBoton left-[835px] top-[-15px] relative">
+          <a href="builders">
+          <button style={{ backgroundColor: '#00BCC6', color: 'white' }}> + Ver Más </button>
+          </a>
+        </div>
+        
+        
+      </div>
+      <div className="VerMas3 w-1219 h-8 top-[630px] relative">
+        <div className="Linea w-1219 h-px border border-neutral-400"></div>
+        <div className="EspacioBoton left-[835px] top-[-15px] relative">
+          <a href="recomendados">
+          <button style={{ backgroundColor: '#00BCC6', color: 'white' }}>+ Ver Más</button>
+          </a>
+        </div>
+      </div>
+      <div className="Personas1 w-1100 h-24 left-[70px] top-[30px] relative">
+        <div className="PersonasInfo w-96 h-24 left-0 top-0 absolute">
+          <div className="Rectangulo w-96 h-24 left-0 top-0 absolute bg-white rounded-xl border border-indigo-950" />
+          <div className="Texto Superior w-40 h-6 left-[79px] top-[19px] absolute text-center text-indigo-950 text-xl font-medium font-['Inter']">NombreUsuario</div>
+          <div className="Texto Inferior w-60 left-[23px] top-[44px] absolute text-center text-indigo-950 text-xl font-medium font-['Inter']">infoUsuario</div>
+          <img className="IconoUser w-10 h-10 left-[30px] top-[25px] absolute" src="src/assets/contarMiembros.png" />
+        </div>
+        <div className="PersonasInfo w-96 h-24 left-[406px] top-0 absolute">
+          <div className="Rectangle88 w-96 h-24 left-0 top-0 absolute bg-white rounded-xl border border-indigo-950" />
+          <div className="Nombreusuario w-40 h-6 left-[79px] top-[19px] absolute text-center text-indigo-950 text-xl font-medium font-['Inter']">NombreUsuario</div>
+          <div className="Infousuario w-60 left-[23px] top-[44px] absolute text-center text-indigo-950 text-xl font-medium font-['Inter']">infoUsuario</div>
+          <img className="Iconouser w-10 h-10 left-[30px] top-[25px] absolute" src="src/assets/contarMiembros.png" />
+        </div>
+        <div className="PersonasInfo w-96 h-24 left-[812px] top-0 absolute">
+          <div className="Rectangle88 w-96 h-24 left-0 top-0 absolute bg-white rounded-xl border border-indigo-950" />
+          <div className="Nombreusuario w-40 h-6 left-[79px] top-[19px] absolute text-center text-indigo-950 text-xl font-medium font-['Inter']">NombreUsuario</div>
+          <div className="Infousuario w-60 left-[23px] top-[44px] absolute text-center text-indigo-950 text-xl font-medium font-['Inter']">infoUsuario</div>
+          <img className="Iconouser w-10 h-10 left-[30px] top-[25px] absolute" src="src/assets/contarMiembros.png" />
+        </div>
+        <div className="PersonasInfo w-96 h-24 left-[1218px] top-0 absolute">
+          <div className="Rectangulo w-96 h-24 left-0 top-0 absolute bg-white rounded-xl border border-indigo-950" />
+          <div className="Texto Superior w-40 h-6 left-[79px] top-[19px] absolute text-center text-indigo-950 text-xl font-medium font-['Inter']">NombreUsuario</div>
+          <div className="Texto Inferior w-60 left-[23px] top-[44px] absolute text-center text-indigo-950 text-xl font-medium font-['Inter']">infoUsuario</div>
+          <img className="IconoUser w-10 h-10 left-[30px] top-[25px] absolute" src="src/assets/contarMiembros.png" />
+        </div>
+      </div>
+      <div className="Personas2 w-1100 h-24 left-[70px] top-[150px] relative">
+        <div className="PersonasInfo w-96 h-24 left-0 top-0 absolute">
+          <div className="Rectangulo w-96 h-24 left-0 top-0 absolute bg-white rounded-xl border border-indigo-950" />
+          <div className="Texto Superior w-40 h-6 left-[79px] top-[19px] absolute text-center text-indigo-950 text-xl font-medium font-['Inter']">NombreUsuario</div>
+          <div className="Texto Inferior w-60 left-[23px] top-[44px] absolute text-center text-indigo-950 text-xl font-medium font-['Inter']">infoUsuario</div>
+          <img className="IconoUser w-10 h-10 left-[30px] top-[25px] absolute" src="src/assets/contarMiembros.png" />
+        </div>
+        <div className="PersonasInfo w-96 h-24 left-[406px] top-0 absolute">
+          <div className="Rectangle88 w-96 h-24 left-0 top-0 absolute bg-white rounded-xl border border-indigo-950" />
+          <div className="Nombreusuario w-40 h-6 left-[79px] top-[19px] absolute text-center text-indigo-950 text-xl font-medium font-['Inter']">NombreUsuario</div>
+          <div className="Infousuario w-60 left-[23px] top-[44px] absolute text-center text-indigo-950 text-xl font-medium font-['Inter']">infoUsuario</div>
+          <img className="Iconouser w-10 h-10 left-[30px] top-[25px] absolute" src="src/assets/contarMiembros.png" />
+        </div>
+        <div className="PersonasInfo w-96 h-24 left-[812px] top-0 absolute">
+          <div className="Rectangle88 w-96 h-24 left-0 top-0 absolute bg-white rounded-xl border border-indigo-950" />
+          <div className="Nombreusuario w-40 h-6 left-[79px] top-[19px] absolute text-center text-indigo-950 text-xl font-medium font-['Inter']">NombreUsuario</div>
+          <div className="Infousuario w-60 left-[23px] top-[44px] absolute text-center text-indigo-950 text-xl font-medium font-['Inter']">infoUsuario</div>
+          <img className="Iconouser w-10 h-10 left-[30px] top-[25px] absolute" src="src/assets/contarMiembros.png" />
+        </div>
+        <div className="PersonasInfo w-96 h-24 left-[1218px] top-0 absolute">
+          <div className="Rectangulo w-96 h-24 left-0 top-0 absolute bg-white rounded-xl border border-indigo-950" />
+          <div className="Texto Superior w-40 h-6 left-[79px] top-[19px] absolute text-center text-indigo-950 text-xl font-medium font-['Inter']">NombreUsuario</div>
+          <div className="Texto Inferior w-60 left-[23px] top-[44px] absolute text-center text-indigo-950 text-xl font-medium font-['Inter']">infoUsuario</div>
+          <img className="IconoUser w-10 h-10 left-[30px] top-[25px] absolute" src="src/assets/contarMiembros.png" />
+        </div>
+      </div>
+      <div className="Personas1 w-1100 h-24 left-[70px] top-[280px] relative">
+        <div className="PersonasInfo w-96 h-24 left-0 top-0 absolute">
+          <div className="Rectangulo w-96 h-24 left-0 top-0 absolute bg-white rounded-xl border border-indigo-950" />
+          <div className="Texto Superior w-40 h-6 left-[79px] top-[10px] absolute text-center text-indigo-950 text-xl font-medium font-['Inter']">NombreUsuario</div>
+          <div className="Texto Medio w-60 left-[23px] top-[30px] absolute text-center text-indigo-950 text-xl font-medium font-['Inter']">infoUsuario</div>
+          <div className="Texto Inferior w-60 left-[73px] top-[50px] absolute text-center text-indigo-950 text-xl font-medium font-['Inter']">Cnt de amigos mutuos: x</div>
+          <img className="IconoUser w-10 h-10 left-[30px] top-[25px] absolute" src="src/assets/contarMiembros.png" />
+        </div>
+        <div className="PersonasInfo w-96 h-24 left-[406px] top-0 absolute">
+          <div className="Rectangulo w-96 h-24 left-0 top-0 absolute bg-white rounded-xl border border-indigo-950" />
+          <div className="Texto Superior w-40 h-6 left-[79px] top-[10px] absolute text-center text-indigo-950 text-xl font-medium font-['Inter']">NombreUsuario</div>
+          <div className="Texto Medio w-60 left-[23px] top-[30px] absolute text-center text-indigo-950 text-xl font-medium font-['Inter']">infoUsuario</div>
+          <div className="Texto Inferior w-60 left-[73px] top-[50px] absolute text-center text-indigo-950 text-xl font-medium font-['Inter']">Cnt de amigos mutuos: x</div>
+          <img className="IconoUser w-10 h-10 left-[30px] top-[25px] absolute" src="src/assets/contarMiembros.png" />
+        </div>
+        <div className="PersonasInfo w-96 h-24 left-[812px] top-0 absolute">
+          <div className="Rectangulo w-96 h-24 left-0 top-0 absolute bg-white rounded-xl border border-indigo-950" />
+          <div className="Texto Superior w-40 h-6 left-[79px] top-[10px] absolute text-center text-indigo-950 text-xl font-medium font-['Inter']">NombreUsuario</div>
+          <div className="Texto Medio w-60 left-[23px] top-[30px] absolute text-center text-indigo-950 text-xl font-medium font-['Inter']">infoUsuario</div>
+          <div className="Texto Inferior w-60 left-[73px] top-[50px] absolute text-center text-indigo-950 text-xl font-medium font-['Inter']">Cnt de amigos mutuos: x</div>
+          <img className="IconoUser w-10 h-10 left-[30px] top-[25px] absolute" src="src/assets/contarMiembros.png" />
+        </div>
+        <div className="PersonasInfo w-96 h-24 left-[1218px] top-0 absolute">
+          <div className="Rectangulo w-96 h-24 left-0 top-0 absolute bg-white rounded-xl border border-indigo-950" />
+          <div className="Texto Superior w-40 h-6 left-[79px] top-[10px] absolute text-center text-indigo-950 text-xl font-medium font-['Inter']">NombreUsuario</div>
+          <div className="Texto Medio w-60 left-[23px] top-[30px] absolute text-center text-indigo-950 text-xl font-medium font-['Inter']">infoUsuario</div>
+          <div className="Texto Inferior w-60 left-[73px] top-[50px] absolute text-center text-indigo-950 text-xl font-medium font-['Inter']">Cnt de amigos mutuos: x</div>
+          <img className="IconoUser w-10 h-10 left-[30px] top-[25px] absolute" src="src/assets/contarMiembros.png" />
+        </div>
+      </div>
+      <main></main>
+    </div>
+
+  );
+};
+
